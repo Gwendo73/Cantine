a126bf24104b30e0be5bf9277b8560fb2c223350
clemphppe <clemgym55@orange.fr>
2022-06-07 17:04:04 +0200

    on a bien avancé

--- cantine.py
+++ cantine.py
@@ -1,7 +1,57 @@
-from flask import Flask, render_template
+from ast import If
+from pydoc import render_doc
+from flask import Flask, redirect, render_template, request,g
+import sqlite3
 
 app = Flask(__name__)
+#IMPORT DE LA BASE DE DONNÉE
 
-@app.route('/')
-def cantine():
-    return render_template("main.html")
+DATABASE = "db/cantine.db"
+
+def get_db():
+    if 'db' not in g:
+        g.db = sqlite3.connect(DATABASE)
+    return g.db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = g.pop('db', None)
+    if db is not None:
+        db.close()
+
+#FENETRE
+
+@app.route('/', methods = ["POST", "GET"])
+def connexion():
+    if request.method== "POST":
+        return render_template("accueil.html")
+    return render_template("pageconnexion.html")
+
+
+@app.route('/pageinscription', methods = ["POST", "GET"])
+def inscription():
+    if request.method== "POST":
+        return render_template("accueil.html")
+    return render_template("pageinscription.html")
+
+@app.route('/enfant')
+def enfant():
+    db = get_db()
+    cur = db.cursor()
+    enfants = cur.execute("SELECT prenom_enfant from enfant WHERE code_representant=1").fetchall()
+    return render_template("enfant.html", enfants=enfants)
+
+@app.route('/facture')
+def facture():
+    mois=["janvier","février","mars","avril","mai","juin","juillet","aout","septembre","octobre","novembre","décembre"];
+    return render_template("facture.html", mois=mois)
+
+@app.route('/actu')
+def actu():
+    return render_template("actualites.html")
+
+@app.route('/deconnexion')
+def deconnexion():
+    return render_template("deconnexion.html")
+
+
