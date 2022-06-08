5c4a71087c18e2ec39683fc7db007a755eaf4124
Gwendal <gwendal.artalle73@gmail.com>
2022-06-08 19:42:07 +0200

    CONNEXION

--- cantine.py
+++ cantine.py
@@ -1,10 +1,33 @@
-from ast import If
-from pydoc import render_doc
 from flask import Flask, redirect, render_template, request,g
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
+import flask_login
+from flask_bcrypt import Bcrypt
 import sqlite3
 
 app = Flask(__name__)
-#IMPORT DE LA BASE DE DONNÉE
+bcrypt = Bcrypt(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+app.config['SECRET_KEY'] = 'clesecrete'
+
+@login_manager.user_loader
+def load_user(user_id):
+    db = get_db()
+    cur = db.cursor()
+    user = cur.execute('SELECT identifiant, mot_de_passe FROM Compte WHERE identifiant=?', (user_id, )).fetchone()
+    user = User(user[0], user[1])
+    return user
+
+class User(UserMixin):
+
+    def __init__(self, username, password):
+        self.name = username
+        self.password = password
+
+    @property
+    def id(self):
+        return self.name
 
 DATABASE = "db/cantine.db"
 
@@ -23,20 +46,34 @@ def close_connection(exception):
 
 @app.route('/', methods = ["POST", "GET"])
 def connexion():
-    db = get_db()
-    cur = db.cursor()
-    if request.method== "POST":
-        return render_template("accueil.html")
-    return render_template("pageconnexion.html")
+    if request.method == "POST":
+        db = get_db()
+        cur = db.cursor()
+        user = cur.execute("SELECT identifiant, mot_de_passe FROM Compte WHERE identifiant=?", (request.form["mail"],)).fetchone()
+        new_user = User(user[0], user[1])
+        if new_user:
+            if bcrypt.check_password_hash(new_user.password, request.form["password"]):
+                login_user(new_user)
+                return redirect("accueil")
+    return render_template("connexion.html")
 
 
-@app.route('/pageinscription', methods = ["POST", "GET"])
+@app.route('/inscription', methods = ["POST", "GET"])
 def inscription():
     if request.method== "POST":
-        return render_template("accueil.html")
-    return render_template("pageinscription.html")
+        db = get_db()
+        cur = db.cursor()
+        password = bcrypt.generate_password_hash(request.form["password"])
+        cur.execute("INSERT INTO Compte(identifiant, mot_de_passe, type_compte) VALUES (?,?,?)",
+            (request.form["mail"], password, "Representant"))
+        cur.execute("INSERT INTO Representant(nom_representant, prenom_representant, telephone, email) VALUES (?,?,?,?)",
+            (request.form["name"], request.form["prenom"], request.form["tel"], request.form["mail"]))
+        db.commit()
+        return redirect("/")
+    return render_template("inscription.html")
 
 @app.route('/enfant')
+@login_required
 def enfant():
     db = get_db()
     cur = db.cursor()
@@ -44,20 +81,35 @@ def enfant():
     return render_template("enfant.html", enfants=enfants)
 
 @app.route('/facture')
+@login_required
 def facture():
     mois=["Janvier","Février","Mars","Avril","Mai","Juin","Juillet","Aout","Septembre","Octobre","Novembre","Décembre"]
     return render_template("facture.html", mois=mois)
 
 @app.route('/actu')
+@login_required
 def actu():
-    return render_template("actualites.html")
+    db = get_db()
+    cur = db.cursor()
+    enfants = cur.execute("SELECT prenom_enfant from enfant WHERE code_representant=1").fetchall()
+    return render_template("actualites.html", enfants = enfants)
 
 @app.route('/info')
+@login_required
 def info():
     return render_template("info.html")
 
-@app.route('/deconnexion')
+@app.route('/deconnexion', methods=['GET', 'POST'])
+@login_required
 def deconnexion():
-    return render_template("deconnexion.html")
+    logout_user()
+    return redirect("/")
 
+@app.route('/accueil')
+@login_required
+def accueil():
+    db = get_db()
+    cur = db.cursor()
+    enfants = cur.execute("SELECT prenom_enfant FROM Enfant AS E INNER JOIN Representant AS R ON E.code_representant = R.code_representant WHERE R.email=?", (flask_login.current_user.name, )).fetchall()
+    return render_template("accueil.html", enfants = enfants)
 
