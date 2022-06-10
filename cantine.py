c458b12a440aa532846eab933a5e1e742ffa47cc
Gwendal <gwendal.artalle73@gmail.com>
2022-06-10 19:29:17 +0200

    encrypt

--- cantine.py
+++ cantine.py
@@ -33,17 +33,19 @@ class User(UserMixin):
 
 @app.route('/', methods = ["POST", "GET"])
 def connexion():
+    # db = get_db()
+    # cur = db.cursor()
+    # users = cur.execute("SELECT * FROM Compte").fetchall()
+    # for user in users:
+    #     password = bcrypt.generate_password_hash(user[1])
+    #     cur.execute("UPDATE Compte SET mot_de_passe = ? WHERE identifiant = ?", (password, user[0],) )
+    #     db.commit()
     if request.method == "POST":
         db = get_db()
         cur = db.cursor()
         user = cur.execute("SELECT * FROM Compte WHERE identifiant=?", (request.form["identifiant"], )).fetchone()
         if user:
             new_user = User(user[0], user[1])
-            try:
-                new_user.password = user[1]
-            except Exception:
-                error = 'Invalid Username or Password'
-                return render_template('connexion.html', error=error)
 
             if bcrypt.check_password_hash(new_user.password, request.form["password"]):
                 login_user(new_user)
