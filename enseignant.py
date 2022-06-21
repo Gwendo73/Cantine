a1d4914e09b9a3090a5e6e1982dc2082de798f5a
Gwendal <gwendal.artalle73@gmail.com>
2022-06-21 14:16:29 +0200

    DEMO FINALE

--- enseignant.py
+++ enseignant.py
@@ -68,7 +68,7 @@ def presence(code_classe):
             code = classes[0][0]
         else:
             code = code_classe
-        enfants = cur.execute("SELECT E.nom_enfant, E.prenom_enfant, E.code_enfant, Rep.telephone, R.code_repas FROM Enseigne AS Ens INNER JOIN Enfant AS E ON Ens.code_classe = E.code_classe INNER JOIN Repas AS R ON R.code_enfant = E.code_enfant INNER JOIN Representant AS Rep ON Rep.code_representant = E.code_representant WHERE R.date_repas = ? AND E.code_classe = ?", (now, code, )).fetchall()
+        enfants = cur.execute("SELECT E.nom_enfant, E.prenom_enfant, E.code_enfant, Rep.telephone, R.code_repas FROM Enseigne AS Ens INNER JOIN Enfant AS E ON Ens.code_classe = E.code_classe INNER JOIN Repas AS R ON R.code_enfant = E.code_enfant INNER JOIN Representant AS Rep ON Rep.code_representant = E.code_representant WHERE R.date_repas = ? AND E.code_classe = ? ORDER BY E.nom_enfant, E.prenom_enfant", (now, code, )).fetchall()
         classeActuelle = cur.execute("SELECT C.code_classe, C.nom_classe FROM Classe AS C INNER JOIN Enseigne AS E ON E.code_classe = C.code_classe WHERE E.code_enseignant = ? AND C.code_classe = ?", (user[0], code, )).fetchone()
         for enfant in enfants:
             allergies.append(cur.execute("SELECT Al.nom_allergie, A.code_enfant FROM EstAllergiqueA AS A INNER JOIN Allergie AS Al ON Al.code_allergie = A.code_allergie WHERE A.code_enfant = ?", (enfant[2], )).fetchall())
